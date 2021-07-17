8332e6dfd14efb08d56a6f8a0db4008097224e6e
surendra-1390 <87278812+surendra-1390@users.noreply.github.com>
2021-07-18 00:22:24 +0530

    Update prod.js

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,5 @@
 
 module.exports = {
-    MONGOURI: 'mongodb+srv://surendra:surendra1390@cluster0.inwfp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    JWT_SECRET:'surendraakshintala',
-    SEND_MAIL_API_KEY: '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'
+    MONGOURI: process.env.MONGOURI,
+    JWT_SECRET: process.env.JWT_SEC
 }
